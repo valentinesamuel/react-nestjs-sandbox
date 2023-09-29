a193cd21e9c38f717d47c25e6eab9e66fe6a90e3
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-09-29 05:28:41 +0100

    Added package files

--- reactjs/src/App.tsx
+++ reactjs/src/App.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { io } from "socket.io-client";
 
 const App: React.FC = () => {
-  return <h1>Ready</h1>;
+  const socket = io("http://localhost:3000");
+
+  const handleEmit = () => {
+    socket.emit("message", { first: "data" });
+  };
+
+  // socket.on("exception", (response) => {
+  //   console.log(response);
+  // });
+  useEffect(() => {
+    // Clean up the WebSocket connection when the component unmounts
+    // return () => {
+    //   socket.disconnect();
+    // };
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  return (
+    <div>
+      <h1>WebSocket Example</h1>
+      <p>Check the browser console for WebSocket messages.</p>
+      <button onClick={handleEmit}>Emmitt</button>
+    </div>
+  );
 };
 
 export default App;
