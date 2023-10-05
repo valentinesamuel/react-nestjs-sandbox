703a5d425918fc5129a606cf8aba3495f48e5c5a
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-10-05 22:17:24 +0100

    feat: sending test result for the doctor listening to it.

--- reactjs/src/App.tsx
+++ reactjs/src/App.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import axios from "axios";
+import Scientist from "./Scientist";
+import { useEffect, useRef } from "react";
+import { Socket, io } from "socket.io-client";
 
 const App: React.FC = () => {
-  return <h1>Ready</h1>;
+  const socketRef = useRef<Socket | null>(null);
+
+  const handleEmit = async () => {
+    const response = await axios.post("//localhost:3000/submit", {
+      doctor: "Solomon",
+      patient: "Makinde",
+      scientist: "Asake",
+    });
+    console.log(response);
+  };
+
+  useEffect(() => {
+    socketRef.current = io("localhost:3000");
+
+    socketRef.current.on("connect", () => {
+      console.log("Doctor connected to the Socket.IO server");
+    });
+
+    socketRef.current.on("sci-done-test", (testResult) => {
+      console.log(testResult);
+    });
+
+    return () => {
+      if (socketRef.current) {
+        console.log("Disconnected");
+        socketRef.current.disconnect();
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Ready</h1>
+      <button onClick={handleEmit}>Request Test</button>
+      <br />
+      <br />
+      <br />
+      <Scientist />
+    </div>
+  );
 };
 
 export default App;
---
+++ reactjs/src/Scientist.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from "react";
+import { io, Socket } from "socket.io-client";
+
+const Scientist = () => {
+  const socketRef = useRef<Socket | null>(null);
+
+  const finishTest = () => {
+    socketRef.current?.emit("test-done", {
+      doctor: "Solomon",
+      patient: "Makinde",
+      scientist: "Asake",
+      result: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+    });
+  };
+
+  useEffect(() => {
+    // Initialize the socket connection when the component mounts
+    socketRef.current = io("localhost:3000");
+
+    // Connect to the WebSocket server
+    socketRef.current.on("connect", () => {
+      console.log("Scientist onnected to the Socket.IO server");
+    });
+    // Listen for incoming messages
+    socketRef.current.on("newTestRequest", (testRequest) => {
+      // Display the test request notification to the lab scientist
+      console.log(testRequest);
+    });
+
+    // Clean up the socket connection when the component unmounts
+    return () => {
+      if (socketRef.current) {
+        console.log("Disconnected");
+        socketRef.current.disconnect();
+      }
+    };
+  }, []);
+  return (
+    <div>
+      <button onClick={finishTest}>Finish Test</button>
+    </div>
+  );
+};
+
+export default Scientist;
