24ba1335958bae5668aa0eb3d7109f3db3ad3218
valentinesamuel2580 <valentinesamuel2580@gmail.com>
2023-12-15 23:44:35 +0100

    switched animation component

--- reactjs/src/App.tsx
+++ reactjs/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import SimpleAnimations from "./components/Animations/SimpleAnimations";
 import VariantsAnimations from "./components/Animations/Variants";
 import MannualAnimations from "./components/Animations/MannualAnimations";
-import SingleValueAnimation from "./components/SingleValueAnimation";
+import SingleValueAnimation from "./components/Animations/SingleValueAnimation";
 
 const App: React.FC = () => {
   return <SingleValueAnimation />;
