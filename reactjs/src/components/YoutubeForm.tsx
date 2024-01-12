6a0e5e87389f7fe78c6e14fda6fccbf1129620d5
valentinesamuel <valentinesamuel2580@gmail.com>
2024-01-12 15:52:52 +0100

    feat: added default values

--- reactjs/src/components/YoutubeForm.tsx
+++ reactjs/src/components/YoutubeForm.tsx
@@ -9,7 +9,24 @@ type FormValues = {
 };
 
 export const YouTubeForm = () => {
-  const form = useForm<FormValues>();
+  const form = useForm<FormValues>({
+    defaultValues: {
+      username: "",
+      email: "",
+      channel: "",
+    },
+    // defaultValues: async () => {
+    //   const response = await fetch(
+    //     "https://jsonplaceholder.typicode.com/users/1"
+    //   );
+    //   const data = await response.json();
+    //   return {
+    //     username: "Batman",
+    //     email: data.email,
+    //     channel: "",
+    //   };
+    // },
+  });
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
 
