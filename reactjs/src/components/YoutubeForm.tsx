c2a644d2ce62c2c70dd60d4dfef7ded06dbc55b0
valentinesamuel <valentinesamuel2580@gmail.com>
2024-01-12 16:45:10 +0100

    feat: added dynamic form components

--- reactjs/src/components/YoutubeForm.tsx
+++ reactjs/src/components/YoutubeForm.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 type FormValues = {
@@ -10,6 +10,8 @@ type FormValues = {
     twitter: string;
     facebook: string;
   };
+  phoneNumbers: string[];
+  phNumbers: { number: string }[];
 };
 
 export const YouTubeForm = () => {
@@ -22,6 +24,8 @@ export const YouTubeForm = () => {
         facebook: "",
         twitter: "",
       },
+      phoneNumbers: ["23424346", "3453465466", "3637894867"],
+      phNumbers: [{ number: "23424346" }, { number: "3453465466" }],
     },
     // defaultValues: async () => {
     //   const response = await fetch(
@@ -37,6 +41,10 @@ export const YouTubeForm = () => {
   });
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "phNumbers",
+  });
 
   const onsubmitForm = (data: FormValues) => {
     console.log("Form Submitted", data);
@@ -106,17 +114,103 @@ export const YouTubeForm = () => {
         </div>
 
         <div className="form-control">
-          <label htmlFor="social">Social</label>
+          <div className="form-control">
+            <label htmlFor="primary-phone">primary Phone number</label>
+            <input
+              type="text"
+              id="primary-phone"
+              {...register("phoneNumbers.0", {
+                validate: {
+                  badNumber: (field) => {
+                    return (
+                      !field.endsWith(String(4)) || "End with a good number"
+                    );
+                  },
+                },
+              })}
+            />
+            <p className="error">{errors.phoneNumbers?.[0]?.message}</p>
+          </div>
+
+          <div className="form-control">
+            <label htmlFor="secondary-phone">Secondary Phone number</label>
+            <input
+              type="text"
+              id="secondary-phone"
+              {...register("phoneNumbers.1", {
+                required: {
+                  value: true,
+                  message: "number is required",
+                },
+              })}
+            />
+            <p className="error">{errors.phoneNumbers?.[0]?.message}</p>
+          </div>
+          <label htmlFor="social">Twitter</label>
           <input type="text" id="social" {...register("social.twitter")} />
           <p className="error">{errors.social?.twitter?.message}</p>
         </div>
 
         <div className="form-control">
-          <label htmlFor="social">Social</label>
+          <label htmlFor="social">Facebook</label>
           <input type="text" id="social" {...register("social.facebook")} />
           <p className="error">{errors.social?.facebook?.message}</p>
         </div>
 
+        <div className="form-control">
+          <label htmlFor="primary-phone">primary Phone number</label>
+          <input
+            type="text"
+            id="primary-phone"
+            {...register("phoneNumbers.0", {
+              validate: {
+                badNumber: (field) => {
+                  return !field.endsWith(String(4)) || "End with a good number";
+                },
+              },
+            })}
+          />
+          <p className="error">{errors.phoneNumbers?.[0]?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="secondary-phone">Secondary Phone number</label>
+          <input
+            type="text"
+            id="secondary-phone"
+            {...register("phoneNumbers.1", {
+              required: {
+                value: true,
+                message: "number is required",
+              },
+            })}
+          />
+          <p className="error">{errors.phoneNumbers?.[1]?.message}</p>
+        </div>
+
+        <div>
+          <label>List of phone numbers</label>
+          <div>
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id} className="form-control">
+                  <input
+                    type="text"
+                    {...register(`phNumbers.${index}.number` as const)}
+                  />
+                  {index > 0 && (
+                    <button type="button" onClick={() => remove(index)}>
+                      Delete
+                    </button>
+                  )}
+                </div>
+              );
+            })}
+            <button type="button" onClick={() => append({ number: "" })}>
+              Add phone number
+            </button>
+          </div>
+        </div>
         <button>Submit</button>
       </form>
       <DevTool control={control} />
